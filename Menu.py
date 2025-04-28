6c99e68abf850ff420b0d4cbce4abbb40cfee489
FabianPalaciosM <fabianpm29@gmail.com>
2025-04-28 01:12:19 -0600

    First commit

---
+++ Menu.py
@@ -0,0 +1,20 @@
+with open("estudiantes.txt", r, encoding="utf-8")as archivo:
+while True:
+    menu = int(input("""
+    1. Crear BD dinamica
+    2. Registrar un estudiante            
+    3. Generar reporte HTML y .csv
+    4. Respaldar en XML
+    5. Reporte por género (.docx)
+    6. Gestionar curva
+    7. Envio de correo para reposición
+    8. Aplazados en al menos 2 exámentes (.pdf)
+    9. Estadística por generación
+    10. Reporte por generación
+    11. Salir
+    """))
+    if menu == 1:
+
+    elif menu == 2:
+
+    elif menu == 3:
